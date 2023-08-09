c83e8d47e6e4c344534095ff70dafb77779a6165
KarolPiasnik <piasnik.karol@outlook.com>
2023-08-09 20:28:46 +0200

    add constraint for code field in product

--- model/product.go
+++ model/product.go
@@ -7,7 +7,7 @@ import (
 type Product struct {
 	gorm.Model
 	ID         uint64   `json:"ID" sql:"AUTO_INCREMENT" gorm:"primary_key"`
-	Code       string   `json:"code"`
+	Code       string   `json:"code" gorm:"unique"`
 	Price      uint     `json:"price"`
 	Name       string   `json:"name"`
 	CategoryID uint64   `json:"categoryID"`
